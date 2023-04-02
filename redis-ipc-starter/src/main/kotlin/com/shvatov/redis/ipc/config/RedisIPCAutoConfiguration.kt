75aa53d5c6a8efbb9619f7ec4942f56bf6d9720a
ishvatov <ishvatov@gmail.com>
2023-04-02 11:27:51 +0300

    Made configuration fully reactive

--- redis-ipc-starter/src/main/kotlin/com/shvatov/redis/ipc/config/RedisIPCAutoConfiguration.kt
+++ redis-ipc-starter/src/main/kotlin/com/shvatov/redis/ipc/config/RedisIPCAutoConfiguration.kt
@@ -1,24 +1,27 @@
 package com.shvatov.redis.ipc.config
 
 import com.shvatov.redis.ipc.config.RedisIPCAutoConfiguration.RedisCommonConfiguration
-import com.shvatov.redis.ipc.config.RedisIPCAutoConfiguration.RedisCommonConfiguration.Companion.REDIS_IPC_TASK_EXECUTOR_BEAN
 import com.shvatov.redis.ipc.config.RedisIPCAutoConfiguration.RedisListenerConfiguration
 import com.shvatov.redis.ipc.config.RedisIPCAutoConfiguration.RedisPublisherConfiguration
-import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.beans.factory.annotation.Value
+import org.springframework.boot.autoconfigure.AutoConfiguration
+import org.springframework.boot.autoconfigure.data.redis.RedisReactiveAutoConfiguration
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.DependsOn
 import org.springframework.context.annotation.Import
-import org.springframework.core.task.TaskExecutor
-import org.springframework.data.redis.connection.RedisConnectionFactory
-import org.springframework.data.redis.core.RedisTemplate
-import org.springframework.data.redis.listener.RedisMessageListenerContainer
+import org.springframework.data.redis.connection.ReactiveRedisConnectionFactory
+import org.springframework.data.redis.core.ReactiveRedisOperations
+import org.springframework.data.redis.core.ReactiveRedisTemplate
+import org.springframework.data.redis.listener.ReactiveRedisMessageListenerContainer
+import org.springframework.data.redis.serializer.RedisSerializationContext
 import org.springframework.data.redis.serializer.RedisSerializer
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor
+import reactor.core.scheduler.Schedulers
 
 /**
  * @see [org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration]
+ * @see [org.springframework.boot.autoconfigure.data.redis.RedisReactiveAutoConfiguration]
  */
 @Configuration("redis.ipc.auto-configuration")
 @Import(
@@ -28,42 +31,47 @@ import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor
         RedisPublisherConfiguration::class,
     ]
 )
+@AutoConfiguration(after = [RedisReactiveAutoConfiguration::class])
 class RedisIPCAutoConfiguration {
 
     @Configuration("redis.ipc.common-configuration")
     class RedisCommonConfiguration {
 
         @Bean(REDIS_IPC_TEMPLATE_BEAN)
-        fun redisIpcTemplate(redisConnectionFactory: RedisConnectionFactory) =
-            RedisTemplate<String, ByteArray>().apply {
-                // connection factory
-                setConnectionFactory(redisConnectionFactory)
-
-                // serialization
-                isEnableDefaultSerializer = true
-                keySerializer = RedisSerializer.string()
-                valueSerializer = RedisSerializer.byteArray()
-            }
-
-        @Bean(REDIS_IPC_TASK_EXECUTOR_BEAN)
-        fun redisIpcTaskExecutor(
-            @Value("\${spring.data.redis.task-executor.core-pool-size:1}") corePoolSize: Int,
-            @Value("\${spring.data.redis.task-executor.max-pool-size:25}") maxPoolSize: Int,
-            @Value("\${spring.data.redis.task-executor.keep-alive-seconds:60}") keepAliveSeconds: Int,
-            @Value("\${spring.data.redis.task-executor.queue-capacity:10000}") queueCapacity: Int,
-        ) = ThreadPoolTaskExecutor().apply {
-            setThreadNamePrefix(IPC_THREAD_POOL_PREFIX)
-            setKeepAliveSeconds(keepAliveSeconds)
-            setQueueCapacity(queueCapacity)
-            setCorePoolSize(corePoolSize)
-            setMaxPoolSize(maxPoolSize)
+        fun reactiveRedisIpcTemplate(
+            reactiveRedisConnectionFactory: ReactiveRedisConnectionFactory
+        ): ReactiveRedisOperations<String, ByteArray> {
+            val serializationContext = RedisSerializationContext.newSerializationContext<String, ByteArray>()
+                .key(RedisSerializer.string())
+                .hashKey(RedisSerializer.string())
+                .value(RedisSerializer.byteArray())
+                .hashValue(RedisSerializer.byteArray())
+                .build()
+            return ReactiveRedisTemplate(reactiveRedisConnectionFactory, serializationContext)
         }
+
+        @Bean(REDIS_IPC_SCHEDULER_BEAN)
+        fun reactiveRedisIpcScheduler(
+            @Value("\${spring.data.redis.task-executor.max-pool-size:25}") maxPoolSize: Int,
+            @Value("\${spring.data.redis.task-executor.core-pool-size:1}") corePoolSize: Int,
+            @Value("\${spring.data.redis.task-executor.queue-capacity:10000}") queueCapacity: Int,
+            @Value("\${spring.data.redis.task-executor.keep-alive-seconds:60}") keepAliveSeconds: Int,
+        ) = Schedulers.fromExecutor(
+            ThreadPoolTaskExecutor().apply {
+                setThreadNamePrefix(IPC_THREAD_POOL_PREFIX)
+                setKeepAliveSeconds(keepAliveSeconds)
+                setQueueCapacity(queueCapacity)
+                setCorePoolSize(corePoolSize)
+                setMaxPoolSize(maxPoolSize)
+            }.also {
+                it.initialize()
+            })
 
         internal companion object {
             private const val IPC_THREAD_POOL_PREFIX = "redis-ipc"
 
-            const val REDIS_IPC_TEMPLATE_BEAN = "redisIpcTemplate"
-            const val REDIS_IPC_TASK_EXECUTOR_BEAN = "redisIpcTaskExecutor"
+            const val REDIS_IPC_TEMPLATE_BEAN = "reactiveRedisIpcTemplate"
+            const val REDIS_IPC_SCHEDULER_BEAN = "reactiveRedisIpcScheduler"
         }
     }
 
@@ -72,16 +80,12 @@ class RedisIPCAutoConfiguration {
     class RedisListenerConfiguration {
 
         @Bean(REDIS_IPC_MESSAGE_LISTENER_CONTAINER_BEAN)
-        fun redisIpcMessageListenerContainer(
-            connectionFactory: RedisConnectionFactory,
-            @Qualifier(REDIS_IPC_TASK_EXECUTOR_BEAN) taskExecutor: TaskExecutor
-        ) = RedisMessageListenerContainer().apply {
-            setConnectionFactory(connectionFactory)
-            setTaskExecutor(taskExecutor)
-        }
+        fun reactiveRedisIpcMessageListenerContainer(
+            reactiveRedisConnectionFactory: ReactiveRedisConnectionFactory
+        ) = ReactiveRedisMessageListenerContainer(reactiveRedisConnectionFactory)
 
         internal companion object {
-            const val REDIS_IPC_MESSAGE_LISTENER_CONTAINER_BEAN = "redisIpcMessageListenerContainer"
+            const val REDIS_IPC_MESSAGE_LISTENER_CONTAINER_BEAN = "reactiveRedisIpcMessageListenerContainer"
         }
     }
 
