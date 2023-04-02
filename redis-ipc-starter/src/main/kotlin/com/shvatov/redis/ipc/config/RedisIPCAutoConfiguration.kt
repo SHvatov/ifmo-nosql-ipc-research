3fb095a8422b7a71b3934f3b8f0bb1b21bf50414
ishvatov <ishvatov@gmail.com>
2023-04-02 22:13:02 +0300

    Added autoconfiguration for listeners via proxying

--- redis-ipc-starter/src/main/kotlin/com/shvatov/redis/ipc/config/RedisIPCAutoConfiguration.kt
+++ redis-ipc-starter/src/main/kotlin/com/shvatov/redis/ipc/config/RedisIPCAutoConfiguration.kt
@@ -1,8 +1,10 @@
 package com.shvatov.redis.ipc.config
 
+import com.fasterxml.jackson.databind.ObjectMapper
 import com.shvatov.redis.ipc.config.RedisIPCAutoConfiguration.RedisCommonConfiguration
 import com.shvatov.redis.ipc.config.RedisIPCAutoConfiguration.RedisListenerConfiguration
 import com.shvatov.redis.ipc.config.RedisIPCAutoConfiguration.RedisPublisherConfiguration
+import com.shvatov.redis.ipc.registrar.RedisListenerBeanDefinitionRegistrar
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.autoconfigure.AutoConfiguration
 import org.springframework.boot.autoconfigure.data.redis.RedisReactiveAutoConfiguration
@@ -32,7 +34,7 @@ import reactor.core.scheduler.Schedulers
     ]
 )
 @AutoConfiguration(after = [RedisReactiveAutoConfiguration::class])
-class RedisIPCAutoConfiguration {
+internal class RedisIPCAutoConfiguration {
 
     @Configuration("redis.ipc.common-configuration")
     class RedisCommonConfiguration {
@@ -52,10 +54,10 @@ class RedisIPCAutoConfiguration {
 
         @Bean(REDIS_IPC_SCHEDULER_BEAN)
         fun reactiveRedisIpcScheduler(
-            @Value("\${spring.data.redis.task-executor.max-pool-size:25}") maxPoolSize: Int,
-            @Value("\${spring.data.redis.task-executor.core-pool-size:1}") corePoolSize: Int,
-            @Value("\${spring.data.redis.task-executor.queue-capacity:10000}") queueCapacity: Int,
-            @Value("\${spring.data.redis.task-executor.keep-alive-seconds:60}") keepAliveSeconds: Int,
+            @Value("\${spring.data.redis.ipc.scheduler.max-pool-size:25}") maxPoolSize: Int,
+            @Value("\${spring.data.redis.ipc.scheduler.core-pool-size:1}") corePoolSize: Int,
+            @Value("\${spring.data.redis.ipc.scheduler.queue-capacity:10000}") queueCapacity: Int,
+            @Value("\${spring.data.redis.ipc.scheduler.keep-alive-seconds:60}") keepAliveSeconds: Int,
         ) = Schedulers.fromExecutor(
             ThreadPoolTaskExecutor().apply {
                 setThreadNamePrefix(IPC_THREAD_POOL_PREFIX)
@@ -65,7 +67,8 @@ class RedisIPCAutoConfiguration {
                 setMaxPoolSize(maxPoolSize)
             }.also {
                 it.initialize()
-            })
+            }
+        )
 
         internal companion object {
             private const val IPC_THREAD_POOL_PREFIX = "redis-ipc"
@@ -77,6 +80,7 @@ class RedisIPCAutoConfiguration {
 
     @DependsOn("redis.ipc.common-configuration")
     @Configuration("redis.ipc.consumer-configuration")
+    @Import(RedisListenerBeanDefinitionRegistrar::class)
     class RedisListenerConfiguration {
 
         @Bean(REDIS_IPC_MESSAGE_LISTENER_CONTAINER_BEAN)
@@ -84,8 +88,12 @@ class RedisIPCAutoConfiguration {
             reactiveRedisConnectionFactory: ReactiveRedisConnectionFactory
         ) = ReactiveRedisMessageListenerContainer(reactiveRedisConnectionFactory)
 
+        @Bean(REDIS_IPC_OBJECT_MAPPER_BEAN)
+        fun redisIpcObjectMapper() = ObjectMapper()
+
         internal companion object {
             const val REDIS_IPC_MESSAGE_LISTENER_CONTAINER_BEAN = "reactiveRedisIpcMessageListenerContainer"
+            const val REDIS_IPC_OBJECT_MAPPER_BEAN = "redisIpcObjectMapper"
         }
     }
 
