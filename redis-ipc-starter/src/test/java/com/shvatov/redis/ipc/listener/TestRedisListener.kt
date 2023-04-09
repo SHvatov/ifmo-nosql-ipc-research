78f3b736f7bfb08556c1778ba7f1226d53f56ff2
ishvatov <ishvatov@gmail.com>
2023-04-09 16:55:33 +0300

    Publisher support part 3

--- redis-ipc-starter/src/test/java/com/shvatov/redis/ipc/listener/TestRedisListener.kt
+++ redis-ipc-starter/src/test/java/com/shvatov/redis/ipc/listener/TestRedisListener.kt
@@ -1,19 +1,24 @@
 package com.shvatov.redis.ipc.listener
 
 import com.shvatov.redis.ipc.annotation.listener.ChannelName
-import com.shvatov.redis.ipc.annotation.listener.Payload
 import com.shvatov.redis.ipc.annotation.listener.Listener
+import com.shvatov.redis.ipc.annotation.listener.Payload
 import com.shvatov.redis.ipc.dto.TestMessage
+import com.shvatov.redis.ipc.publisher.TestRedisPublisher
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
+import org.springframework.beans.factory.annotation.Autowired
+import java.util.UUID
 import java.util.concurrent.TimeUnit.MILLISECONDS
 
 
 open class TestRedisListener {
 
+    @set:Autowired
+    lateinit var publisher: TestRedisPublisher
+
     @Listener(
-        channelPatterns = ["app.test-channel-pattern"],
-        channels = ["channel-1", "channel-2", "test_topic"],
+        channels = ["test_channel_in"],
         bufferSize = 10,
         bufferingDuration = 500,
         bufferingDurationUnit = MILLISECONDS,
@@ -24,6 +29,10 @@ open class TestRedisListener {
         @Payload(payloadClass = TestMessage::class) message: List<TestMessage>
     ) {
         logger.error("Processing following message from channel {}: {}", channel, message)
+
+        val firstMessage = message.first()
+        publisher.publish(firstMessage.copy(publisherId = UUID.randomUUID(), payload = "Response"))
+            .block()
     }
 
     private companion object {
