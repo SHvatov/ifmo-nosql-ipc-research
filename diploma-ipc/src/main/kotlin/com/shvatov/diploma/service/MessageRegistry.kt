6a16ceec285b15301d84e3177b56ac799cf9df0e
ishvatov <ishvatov@gmail.com>
2023-04-10 20:29:03 +0300

    CI/CD

--- diploma-ipc/src/main/kotlin/com/shvatov/diploma/service/MessageRegistry.kt
+++ diploma-ipc/src/main/kotlin/com/shvatov/diploma/service/MessageRegistry.kt
@@ -2,23 +2,18 @@ package com.shvatov.diploma.service
 
 import com.shvatov.diploma.dto.Message
 import com.shvatov.diploma.util.MESSAGE_ID_KEY
-import io.micrometer.core.instrument.MeterRegistry
-import io.micrometer.core.instrument.Tag
 import org.slf4j.MDC
 import org.springframework.stereotype.Component
 
 @Component
-class MessageRegistry(private val meterRegistry: MeterRegistry) {
+class MessageRegistry {
 
     fun create() = register(Message.create())
         .also { it.applyToMDC() }
 
     @Synchronized
     fun register(message: Message) =
-        message.also {
-            it.applyToMDC()
-            meterRegistry.gauge("message_status", listOf(Tag.of("id", it.id.toString())), it.status)
-        }
+        message.also { it.applyToMDC() }
 }
 
 private fun Message.applyToMDC() {
