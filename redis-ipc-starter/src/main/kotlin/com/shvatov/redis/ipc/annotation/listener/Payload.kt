b134a79722a70aca980c1f997c3daa42f7f5dec6
ishvatov <ishvatov@gmail.com>
2023-04-10 20:37:28 +0300

    Use docker hub image

--- redis-ipc-starter/src/main/kotlin/com/shvatov/redis/ipc/annotation/listener/Payload.kt
+++
@@ -1,7 +0,0 @@
-package com.shvatov.redis.ipc.annotation.listener
-
-import kotlin.reflect.KClass
-
-@Target(AnnotationTarget.VALUE_PARAMETER)
-@Retention(AnnotationRetention.RUNTIME)
-annotation class Payload(val payloadClass: KClass<out Any> = Unit::class)
--- redis-ipc-starter/src/main/kotlin/com/shvatov/redis/ipc/annotation/publisher/PublisherId.kt
+++
@@ -1,5 +0,0 @@
-package com.shvatov.redis.ipc.annotation.publisher
-
-@Target(AnnotationTarget.FIELD)
-@Retention(AnnotationRetention.RUNTIME)
-annotation class PublisherId
